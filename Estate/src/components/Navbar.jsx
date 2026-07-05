e778aefa749654713c2e8ad41da53fc8b9906483
Sanket <sp1761520@gmail.com>
2026-07-05 13:27:44 +0530

    About page completed

--- Estate/src/components/Navbar.jsx
+++ Estate/src/components/Navbar.jsx
@@ -24,12 +24,12 @@ const Navbar = () => {
             <ul className='hidden md:flex gap-7 text-white'>
                 <a href="#Header" className='cursor-pointer hover:text-gray-400'>Home</a>
 
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>About</a>
+                <a href="#About" className='cursor-pointer hover:text-gray-400'>About</a>
 
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Projects</a>
+                <a href="#Projects" className='cursor-pointer hover:text-gray-400'>Projects</a>
 
-                <a href="#Header" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
-                <a href=""></a>
+                <a href="#Testimonials" className='cursor-pointer hover:text-gray-400'>Testimonials</a>
+               
             </ul>
 
             <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign up</button>
